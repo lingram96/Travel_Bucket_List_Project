199d56e158f3d0a86bbbe28ad72324c915512e41
Lewis Ingram <lewisingram@Lewiss-MacBook-Pro-2.local>
2021-09-28 21:52:41 +0100

    eight commit

--- console.py
+++ console.py
@@ -7,31 +7,46 @@ import repositories.city_repository as city_repository
 city_repository.delete_all()
 country_repository.delete_all()
 
-country_1 = Country("Italy", "Rome", "Europe", False, "Shop")
+country_1 = Country("Italy", "Rome", "Europe", True, "Visit the Vatican and Coliseum.")
 country_repository.save(country_1)
 
-country_2 = Country("Canada", "Ottawa", "North America", True, "Shop")
+country_2 = Country("Canada", "Toronto", "North America", True, "Check out the CN Tower and Lake Ontario.")
 country_repository.save(country_2)
 
-country_3 = Country("Portugal", "Lisbon", "Europe", False, "Shop")
+country_3 = Country("Portugal", "Lisbon", "Europe", True, "Ride the cable cars.")
 country_repository.save(country_3)
 
-country_4 = Country("Spain", "Madrid", "Europe", False, "Shop")
+country_4 = Country("Spain", "Madrid", "Europe", True, "Go see a Real Madrid game.")
 country_repository.save(country_4)
 
-city_1 = City("Rome", country_1, "Vatican", False)
+country_5 = Country("Sweden", "Stockholm", "Europe", True, "Check out the Vasa Museum and spend all my money.")
+country_repository.save(country_5)
+
+country_6 = Country("Ireland", "Dublin", "Europe", True, "Have a pint in Temple Bar.")
+country_repository.save(country_6)
+
+country_7 = Country("Morocco", "Rabat", "Africa", False, "Smoke some Moroccan Hash.")
+country_repository.save(country_7)
+
+country_8 = Country("Japan", "Tokyo", "Asia", False, "Eat ramen and fight a sumo wrestler.")
+country_repository.save(country_8)
+
+country_9 = Country("Slovenia", "Ljubljana", "Europe", True, "See the castle.")
+country_repository.save(country_9)
+
+city_1 = City("Rome", country_1, "Vatican", True)
 city_repository.save(city_1)
 
 city_2 = City("Venice", country_1, "Canals", False)
 city_repository.save(city_2)
 
-city_3 = City("Ottawa", country_2, "Parliament", True)
+city_3 = City("Toronto", country_2, "Casa Loma", True)
 city_repository.save(city_3)
 
-city_4 = City("Lisbon", country_3, "Ocean", False)
+city_4 = City("Lisbon", country_3, "Belém Tower", False)
 city_repository.save(city_4)
 
-city_5 = City("Madrid", country_4, "Real Madrid Stadium", False)
+city_5 = City("Seville", country_4, "Royal Palace", False)
 city_repository.save(city_5)
 
 print(country_repository.select_all()[0].id)
